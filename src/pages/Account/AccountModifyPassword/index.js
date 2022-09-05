7d76c24058fd8fa1b848d2cc901140312e58ab87
Carolyn <c920231@gmail.com>
2022-09-05 21:23:10 +0800

    update Account

--- src/pages/Account/AccountModifyPassword/index.js
+++ src/pages/Account/AccountModifyPassword/index.js
@@ -4,7 +4,28 @@ import './index.scss';
 function AccountModifyPassword() {
   return (
     <>
-      <div className="account_password w-100">modify password</div>
+      <div className="account_password w-100">
+        <form className="account_password-form">
+          <div>
+            <label className="form-label">目前密碼</label>
+            <input type="password" className="form-control" />
+          </div>
+          <div>
+            <label className="form-label">新密碼</label>
+            <input type="password" className="form-control" />
+          </div>
+          <div>
+            <label className="form-label">新密碼確認</label>
+            <input type="password" className="form-control" />
+          </div>
+
+          <div>
+            <button className="btn btn btn-primary text-white" type="submit">
+              確定修改
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
