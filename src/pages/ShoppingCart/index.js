f05a216592363f019b7da90045446b6554e5d2f2
Wan <tinklin4@gmail.com>
2022-09-08 15:44:22 +0800

    resolve conflict

--- src/pages/ShoppingCart/index.js
+++ src/pages/ShoppingCart/index.js
@@ -4,7 +4,7 @@ import CartMain from './components/CartMain';
 import CartCoupon from './components/CartCoupon';
 import Shipping from './components/Shipping';
 import SideSection from './components/SideSection';
-import '../../styles/global.scss';
+// import '../../styles/global.scss'; //這行不用下喔，上層有引入了
 import './styles/index.scss';
 
 function ShoppingCart() {
