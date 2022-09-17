066aa389446c80d636c17021a0bea1f8960151ad
Wan <tinklin4@gmail.com>
2022-09-17 16:57:38 +0800

    serch&sort

--- src/pages/Recipes/RecipeDetail/index.js
+++ src/pages/Recipes/RecipeDetail/index.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import {
-  BsSearch,
-  BsFillPersonFill,
-} from 'react-icons/bs';
+import { BsSearch, BsFillPersonFill } from 'react-icons/bs';
 import { BiTimeFive } from 'react-icons/bi';
 import '../styles/RecipesDetail.scss';
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Navigate } from 'react-router-dom';
 import ProgressBar from '@ramonak/react-progress-bar';
 import Rating from '@mui/material/Rating';
 
 function RecipeDetail() {
   const [introData, setIntroData] = useState([]);
   const [ingData, setIngData] = useState([]);
+  const [productData, setProductData] = useState([]);
   const [stepsData, setStepsData] = useState([]);
   const [commentData, setCommentData] = useState([]);
-  const [starInfo, setStarInfo] = useState([]);
+  const [starInfo, setStarInfo] = useState({ gradeAverage: 0 });
   const [gradeInfo, setGradeInfo] = useState([]);
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isSearch, setIsSearch] = useState(false);
 
   const { recipeId } = useParams();
 
@@ -29,6 +30,7 @@ function RecipeDetail() {
       );
       setIntroData(response.data.introData);
       setIngData(response.data.ingData);
+      setProductData(response.data.productData);
       setStepsData(response.data.stepsData);
       setCommentData(response.data.commentData);
       setStarInfo(response.data.starInfo);
@@ -37,13 +39,17 @@ function RecipeDetail() {
     getRecipe();
   }, []);
 
+  if (isSearch) {
+    return <Navigate to={`/recipes/飲品?search=${searchTerm}`} />;
+  }
+
   return (
     <>
       <div className="container recipe-detail-container row">
         <div className="main col-12 col-md-8 col-lg-8 ">
           {introData.map((intro) => {
             return (
-              <div>
+              <div key={intro.id}>
                 <div className="recipe-title ">
                   <h1>{intro.title}</h1>
                   <h4>{intro.intro}</h4>
@@ -81,6 +87,46 @@ function RecipeDetail() {
               })}
             </div>
           </div>
+          <div className="recipe-product ">
+            {productData.length == 0 ? (
+              <></>
+            ) : (
+              <>
+                <h3>推薦商品</h3>
+                <hr />
+              </>
+            )}
+
+            <div className="recipe-recommend-display">
+              {productData.map((product) => {
+                return (
+                  <div key={product.id}>
+                    <div className="card recipe-recommend-card ">
+                      <img
+                        src={require(`../../../Assets/products/${product.image}`)}
+                        className="card-img-top products_list-card-img-top"
+                        alt="..."
+                      />
+
+                      <div className=" card-body products_list-card-body">
+                        <Link
+                          className=" card-title products_list-card-title word-wrap"
+                          to={`/productDetail/${product.product_id}`}
+                        >
+                          {product.name}
+                        </Link>
+
+                        <p className=" card-text products_list-card-text">
+                          NT${product.price}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+
           <div className="recipe-steps mt-5">
             <h3>步驟</h3>
             <hr />
@@ -111,7 +157,6 @@ function RecipeDetail() {
                 <div className="product_detail-average-score">
                   {starInfo.gradeAverage}
                 </div>
-                {/* TODO: fix  */}
                 <div className="product_detail-stars">
                   <Rating
                     name="half-rating-read"
@@ -126,7 +171,7 @@ function RecipeDetail() {
                 <div className="product_detail-score-text">評價分佈顯示</div>
                 {gradeInfo.map((v, i) => {
                   return (
-                    <div className="product_detail-star-bar">
+                    <div key={v.id} className="product_detail-star-bar">
                       <p>
                         {i + 1}顆星({v.gradeCount})
                       </p>
@@ -150,12 +195,12 @@ function RecipeDetail() {
             </div>
             {commentData.map((comment) => {
               return (
-                <div className="review d-flex row">
+                <div key={comment.id} className="review d-flex row">
                   <div className="review-left col-md-2">
                     <BsFillPersonFill size={100} color="#9aab82" />
                   </div>
                   <div className="review-right col-md-10">
-                    <div className="review-name">{comment.name}</div>
+                    <div className="review-name">{comment.user_name}</div>
                     <div className="review-content">{comment.comment}</div>
                     <div className="review-info d-flex justify-content-between">
                       <div className="review-date">{comment.create_date}</div>
@@ -201,8 +246,19 @@ function RecipeDetail() {
                 type="search"
                 placeholder="搜尋食譜"
                 aria-label="Search"
+                value={searchTerm}
+                onChange={(e) => {
+                  setSearchTerm(e.target.value);
+                }}
               />
-              <button class="btn" type="submit">
+              <button
+                class="btn"
+                type="submit"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setIsSearch(true);
+                }}
+              >
                 <BsSearch />
               </button>
             </form>
--- src/pages/Recipes/RecipesList/index.js
+++ src/pages/Recipes/RecipesList/index.js
@@ -4,15 +4,21 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { FaLongArrowAltRight } from 'react-icons/fa';
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 import RecipeNavbar from '../component/RecipeNavbar';
-import { BsSearch } from 'react-icons/bs';
-import _ from 'lodash'
+import { BsSearch, BsSortDownAlt, BsSortDown } from 'react-icons/bs';
+import _ from 'lodash';
 
 function RecipesList() {
+  let [searchParams, setSearchParams] = useSearchParams();
+  let search = searchParams.get('search');
+
   const [data, setData] = useState([]);
-  const [dataAll, setDataAll] = useState([]);
   const { category } = useParams();
+  const [myCategory, setMyCategory] = useState(category);
+  const [total, setTotal] = useState();
+  const [sorts, setSorts] = useState('DESC');
+  const [searchTerm, setSearchTerm] = useState(search);
 
   //總共有lastPage這麼多頁
   const [lastPage, setLastPage] = useState(1);
@@ -20,18 +26,42 @@ function RecipesList() {
   const [page, setPage] = useState(1);
 
   useEffect(() => {
+    console.log('effect category', category);
+    setPage(1);
+    if (!search) {
+      setSearchTerm('');
+      setMyCategory(category);
+    }
+  }, [category]);
+
+  useEffect(() => {
+    console.log('effect searchTerm', searchTerm);
+    if (searchTerm.length > 0) {
+      setMyCategory('search');
+    } else {
+      setMyCategory(category);
+    }
+
+    setPage(1);
+  }, [searchTerm]);
+
+  useEffect(() => {
+    console.log('effect category, page  fe', myCategory, page);
+    window.scrollTo(0, 0);
     let getRecipes = async () => {
-      console.log('category  fe', category);
-      let response = await axios.get(
-        `http://localhost:3002/api/1.0/recipes/${category}?page=${page}`
-      );
+      let url = `http://localhost:3002/api/1.0/recipes/${myCategory}?page=${page}&sorts=${sorts}`;
+      console.log('fe', sorts);
+      if (searchTerm.length > 0) {
+        url += `&search=${searchTerm}`;
+      }
+      let response = await axios.get(url);
       setData(response.data.data);
-      setDataAll(response.data.dataAll);
       setLastPage(response.data.pagination.lastPage);
-      console.log(data);
+      setTotal(response.data.pagination.total);
     };
     getRecipes();
-  }, [category, page]);
+  }, [myCategory, page, searchTerm, sorts]);
+
   // 後端分頁
   const getPages = () => {
     let pages = [];
@@ -107,94 +137,7 @@ function RecipesList() {
 
     return pages;
   };
- 
 
-  // 搜尋功能
-  const [searchTerm, setSearchTerm] = useState('');
-  if (searchTerm !== '') {
-    return (
-      <>
-        <RecipeNavbar />
-        <div className="recipe-list">
-          <div className="container">
-            <div className="row">
-              <div className="recipe-title d-flex justify-content-between py-3">
-                <h1>"{searchTerm}"的搜尋結果</h1>
-                <div>
-                  <h6 className="d-inline-flex">共10篇食譜</h6>
-                  <h6 className="d-inline-flex"> | </h6>
-                  <form
-                    class="d-inline-flex ms-3 d-flex searchbar"
-                    role="search"
-                  >
-                    <input
-                      class="me-2 form-control recipe-form-control"
-                      type="search"
-                      placeholder="搜尋食譜"
-                      aria-label="Search"
-                      onChange={(e) => {
-                        setSearchTerm(e.target.value);
-                      }}
-                    />
-                    <button class="btn" type="submit">
-                      <BsSearch />
-                    </button>
-                  </form>
-                </div>
-              </div>
-
-              {dataAll
-                .filter((v) => {
-                  if (v.title.includes(searchTerm)) {
-                    return v;
-                  }
-                })
-                .map((recipe) => {
-                  return (
-                    <div
-                      key={recipe.id}
-                      className="col-12 col-md-6 col-lg-4 d-flex justify-content-center"
-                    >
-                      <div className="recipe-card ">
-                        <div className="recipe_img">
-                          <img
-                            src={require(`../Asset/recipe-image/${recipe.main_img}`)}
-                          ></img>
-                        </div>
-                        <div className="recipe-card-body">
-                          <div className="recipe_date">
-                            {recipe.create_time}
-                          </div>
-                          <div className="recipe_title">
-                            <h4>
-                              <Link
-                                className="recipe-card-title"
-                                to={`/recipeDetail/${recipe.recipe_id}`}
-                              >
-                                {recipe.title}
-                              </Link>
-                            </h4>
-                          </div>
-                          <div className="recipe_content text-truncate">
-                            {recipe.intro}
-                          </div>
-                          <div className="recipe-readmore">
-                            <Link to="/recipes/recipeDetail/123">
-                              閱讀更多
-                              <FaLongArrowAltRight />
-                            </Link>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })}
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  }
   return (
     <>
       <RecipeNavbar />
@@ -202,19 +145,36 @@ function RecipesList() {
         <div className="container">
           <div className="row">
             <div className="recipe-title d-flex justify-content-between py-3">
-              <h1>{category}</h1>
+              <h1>{myCategory === 'search' ? searchTerm : category}</h1>
               <div>
-                <h6 className="d-inline-flex">共10篇食譜</h6>
+                {/* TODO:共幾篇 */}
+                <h6 className="d-inline-flex">共 {total} 篇食譜</h6>
                 <h6 className="d-inline-flex"> | </h6>
                 <h6 className="d-inline-flex">依更新時間排序</h6>
+                <h6
+                  className="d-inline-flex"
+                  onClick={() => {
+                    if (sorts == 'DESC') {
+                      setSorts('ASC');
+                    } else {
+                      setSorts('DESC');
+                    }
+                    console.log(sorts);
+                  }}
+                >
+                  {sorts === 'ASC' ? <BsSortDownAlt /> : <BsSortDown />}
+                </h6>
                 <form class="d-inline-flex ms-3 d-flex searchbar" role="search">
                   <input
                     class="me-2 form-control recipe-form-control"
                     type="search"
                     placeholder="搜尋食譜"
                     aria-label="Search"
+                    value={searchTerm}
                     onChange={(e) => {
                       setSearchTerm(e.target.value);
+                      // 改網址
+                      setSearchParams({ search: e.target.value });
                     }}
                   />
                   <button class="btn" type="submit">
@@ -226,7 +186,10 @@ function RecipesList() {
 
             {data.map((recipe) => {
               return (
-                <div className="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
+                <div
+                  key={recipe.id}
+                  className="col-12 col-md-6 col-lg-4 d-flex justify-content-center"
+                >
                   <div className="recipe-card ">
                     <div className="recipe_img">
                       <img
