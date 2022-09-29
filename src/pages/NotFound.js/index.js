a3acb3f13244068f32464c9be7a1ab289443fcf2
Henry <henry194557@gmail.com>
2022-09-29 15:46:15 +0800

    decorate  404 page

--- src/pages/NotFound.js/index.js
+++ src/pages/NotFound.js/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NotFound.scss';
 import { motion } from 'framer-motion';
 
 function NotFound() {
   return (
-    <motion.div
-      className="my-5 fs-1 page-height text-center text-primary"
-      initial={{ scale: 1, y: 0, opacity: 0.5 }}
-      animate={{ scale: 1.5, y: 180, opacity: 1 }}
-      exit={{ scale: 1 }}
-    >
-      404 NotFound
-    </motion.div>
+    <>
+      <motion.div
+        className=" d-flex flex-column my-5 fs-1 page-height text-center text-primary"
+        initial={{ scale: 1, y: 0, opacity: 0.5 }}
+        animate={{ scale: 1.5, y: 180, opacity: 1 }}
+        exit={{ scale: 1 }}
+      >
+        404 NotFound
+        <p className=" mt-1 fs-5">走錯了喔</p>
+        <Link className="text-dark fs-5 mt-3" to="/">
+          回首頁
+        </Link>
+      </motion.div>
+    </>
   );
 }
 
